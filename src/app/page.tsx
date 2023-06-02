480f0dbfb7d72296b558fac6e24684ba547559c2
debbyalmadea <almadeaputri@gmail.com>
2023-06-02 13:34:57 +0800

    add: ui and integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,125 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCheck,
+  faCircleExclamation,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [payload, setPayload] = useState<string>();
+  const [result, setResult] = useState<string>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [isClicked, setIsClicked] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  async function generate() {
+    if (payload) {
+      const { data, error } = await supabase.functions.invoke(
+        "generate-json-example",
+        {
+          body: { payload: payload },
+        }
+      );
+
+      if (data) {
+        setResult(data.example);
+        console.log("payload: ", payload, "data: ", data);
+      }
+      if (error) {
+        setErrorMessage(error.message);
+        console.log(error);
+      }
+    }
+  }
+
+  function copyResult() {
+    navigator.clipboard.writeText(result ? result : "");
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 500);
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-start space-y-10 px-24 py-16">
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="text-4xl">
+          <span className="text-green-400 mr-4">{"{ }"}</span>faker
+        </h1>
+        <Link
+          href={"https://github.com/debbyalmadea/faker"}
+          className="px-4 py-2 bg-transparent text-neutral-300 border border-neutral-300 hover:bg-neutral-950 active:bg-neutral-900 rounded-md"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+          <span className="ml-4">See Project</span>
+        </Link>
+      </div>
+      <div className="items-center w-full justify-between font-mono text-sm border-b px-8 rounded-lg bg-gradient-to-b pb-6 pt-8 backdrop-blur-3xl border-neutral-800 bg-zinc-800/30 from-inherit lg:bg-gray-200 lg:bg-zinc-800/30">
+        <div className="flex flex-row space-x-3">
+          <div className="bg-red-400 w-3 h-3 rounded-full" />
+          <div className="bg-yellow-400 w-3 h-3 rounded-full" />
+          <div className="bg-green-400 w-3 h-3 rounded-full" />
+        </div>
+        <textarea
+          autoFocus
+          rows={16}
+          onChange={(e) => {
+            setPayload(e.target.value);
+            console.log(e.target.value);
+          }}
+          className="focus:outline-none w-full bg-transparent pt-8"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button
+        onClick={generate}
+        className="px-4 py-2 bg-green-400 hover:bg-green-300 active:bg-green-500 text-green-950 rounded-md"
+      >
+        Generate
+      </button>
+      {errorMessage && (
+        <div className="flex flex-row px-8 py-4 w-full space-x-4 rounded-md border items-start justify-between border-rose-600 bg-rose-950">
+          <FontAwesomeIcon
+            icon={faCircleExclamation}
+            className="text-rose-400 my-1"
+          />
+          <div className="flex flex-col w-full">
+            <h3 className="font-bold">Failed to generate example</h3>
+            <p>{errorMessage}</p>
+          </div>
+          <FontAwesomeIcon icon={faClose} className="text-rose-400 my-1" />
+        </div>
+      )}
+      {result && (
+        <div className="flex flex-row justify-between w-full">
+          <pre className="font-mono">{result}</pre>
+          <button
+            onClick={copyResult}
+            className={`${
+              !isClicked && "w-10"
+            } h-10 bg-transparent text-green-300 border border-green-300 hover:bg-green-950 active:bg-green-900 rounded-md`}
+          >
+            {isClicked ? (
+              <div className="flex flex-row items-center px-4 space-x-2">
+                <FontAwesomeIcon icon={faCheck} />
+                <p>copied</p>
+              </div>
+            ) : (
+              <FontAwesomeIcon icon={faCopy} />
+            )}
+          </button>
+        </div>
+      )}
     </main>
   );
 }
