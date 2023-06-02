c24c9b3441b13e68dcd3a73bd9ecb94a27ac49ba
debbyalmadea <almadeaputri@gmail.com>
2023-06-03 01:19:01 +0800

    fix: ui in smaller screen, color scheme, and add examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import ExampleCode from "./Example";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -18,13 +21,17 @@ const supabase = createClient(
 );
 
 export default function Home() {
-  const [payload, setPayload] = useState<string>();
-  const [result, setResult] = useState<string>();
+  const [payload, setPayload] = useState<string>(
+    `type Row = {\n   name: string\n}`
+  );
+  const [result, setResult] = useState<string>(`{\n "name": "John Doe"\n}`);
   const [errorMessage, setErrorMessage] = useState<string>();
+  const [loading, setLoading] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
 
   async function generate() {
     if (payload) {
+      setLoading(true);
       const { data, error } = await supabase.functions.invoke(
         "generate-json-example",
         {
@@ -40,6 +47,7 @@ export default function Home() {
         setErrorMessage(error.message);
         console.log(error);
       }
+      setLoading(false);
     }
   }
 
@@ -52,40 +60,54 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-start space-y-10 px-24 py-16">
+    <main className="flex min-h-screen flex-col items-start space-y-10 px-8 lg:px-24 py-16 bg-zinc-900 text-zinc-100 font-sans">
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="text-4xl">
           <span className="text-green-400 mr-4">{"{ }"}</span>faker
         </h1>
         <Link
           href={"https://github.com/debbyalmadea/faker"}
-          className="px-4 py-2 bg-transparent text-neutral-300 border border-neutral-300 hover:bg-neutral-950 active:bg-neutral-900 rounded-md"
+          className="px-4 py-2 bg-transparent text-shark-300 border border-shark-300 hover:bg-shark-950 active:bg-shark-900 rounded-md"
         >
           <FontAwesomeIcon icon={faGithub} />
-          <span className="ml-4">See Project</span>
+          <span className="ml-4 hidden lg:inline">See Project</span>
         </Link>
       </div>
-      <div className="items-center w-full justify-between font-mono text-sm border-b px-8 rounded-lg bg-gradient-to-b pb-6 pt-8 backdrop-blur-3xl border-neutral-800 bg-zinc-800/30 from-inherit lg:bg-gray-200 lg:bg-zinc-800/30">
+      <ExampleCode onClick={setPayload} disabled={loading} />
+      <div className="items-center w-full justify-between font-mono text-sm p-8 bg-shark-950 rounded-md">
         <div className="flex flex-row space-x-3">
-          <div className="bg-red-400 w-3 h-3 rounded-full" />
-          <div className="bg-yellow-400 w-3 h-3 rounded-full" />
-          <div className="bg-green-400 w-3 h-3 rounded-full" />
+          <div className="bg-red-400 w-2 lg:w-3 h-2 lg:h-3 rounded-full" />
+          <div className="bg-yellow-400 w-2 lg:w-3 h-2 lg:h-3 rounded-full" />
+          <div className="bg-green-400 w-2 lg:w-3 h-2 lg:h-3 rounded-full" />
         </div>
         <textarea
           autoFocus
-          rows={16}
+          value={payload}
+          disabled={loading}
+          rows={12}
           onChange={(e) => {
             setPayload(e.target.value);
             console.log(e.target.value);
           }}
-          className="focus:outline-none w-full bg-transparent pt-8"
+          className="focus:outline-none w-full bg-transparent pt-8 text-base"
         />
       </div>
       <button
         onClick={generate}
-        className="px-4 py-2 bg-green-400 hover:bg-green-300 active:bg-green-500 text-green-950 rounded-md"
+        disabled={loading}
+        className={`px-4 py-2 bg-green-400 hover:bg-green-300 active:bg-green-500 ${
+          loading && "bg-green-800 hover:bg-green-800 active:bg-green-800"
+        } text-green-950 rounded-md`}
       >
-        Generate
+        {loading ? (
+          <div className="flex flex-row space-x-2 py-1 px-2">
+            <div className="bg-green-700 w-3 h-3 rounded-full animate-pulse" />
+            <div className="bg-green-600 w-3 h-3 rounded-full animate-pulse" />
+            <div className="bg-green-500 w-3 h-3 rounded-full animate-pulse" />
+          </div>
+        ) : (
+          "Generate"
+        )}
       </button>
       {errorMessage && (
         <div className="flex flex-row px-8 py-4 w-full space-x-4 rounded-md border items-start justify-between border-rose-600 bg-rose-950">
@@ -101,13 +123,19 @@ export default function Home() {
         </div>
       )}
       {result && (
-        <div className="flex flex-row justify-between w-full">
-          <pre className="font-mono">{result}</pre>
+        <div className="relative w-full">
+          <SyntaxHighlighter
+            language="javascript"
+            style={atomDark}
+            customStyle={{ width: "100%", padding: "64px 32px 32px 32px" }}
+          >
+            {result}
+          </SyntaxHighlighter>
           <button
             onClick={copyResult}
             className={`${
               !isClicked && "w-10"
-            } h-10 bg-transparent text-green-300 border border-green-300 hover:bg-green-950 active:bg-green-900 rounded-md`}
+            } absolute right-6 top-8 h-10 bg-transparent text-green-300 border border-green-300 hover:bg-green-950 active:bg-green-900 rounded-md`}
           >
             {isClicked ? (
               <div className="flex flex-row items-center px-4 space-x-2">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        shark: {
+          50: "#f6f6f7",
+          100: "#e2e3e5",
+          200: "#c4c8cb",
+          300: "#9fa5a9",
+          400: "#7a8287",
+          500: "#60676c",
+          600: "#4b5156",
+          700: "#3f4246",
+          800: "#35373a",
+          900: "#2e3133",
+          950: "#1d1f21",
+        },
+      },
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+        mono: ["Inconsolata", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [],
-}
+};
